13b664bf69717be2758dd8d9f2505dff570e3985
OfficalAptx-4869 <133824228+OfficalAptx-4869@users.noreply.github.com>
2023-05-19 23:35:45 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,11 +16,12 @@ read
 clear
 
 #menu
-echo " AptxBasics Tool !"
+echo " Welcome to Aptoxine Tool !"
           
 echo "1 - NPhish Tool"
 echo "2 - zphisher Tool"
 echo "3 - install All termux Basics"
+echo "4 - Change Termux Banner"
 
 #if statment
 
@@ -38,6 +39,19 @@ if [ $user = 1 ]; then
 
 pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrade
         
+      elif [[ $user = 4 ]]; then
+      git clone https://github.com/Bhai4You/Termux-Banner
+
+cd Termux-Banner
+
+chmod +x requirement.sh
+
+chmod +x t-ban.sh
+
+bash requirement.sh
+
+bash t-ban.sh
+
     fi
 
 }
