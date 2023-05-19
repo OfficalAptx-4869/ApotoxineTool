f7d4dca23a1e602380c36a6a66278160d0458c3b
OfficalAptx-4869 <133824228+OfficalAptx-4869@users.noreply.github.com>
2023-05-19 20:13:22 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,40 +17,42 @@ clear
 
 #menu
 echo " AptxBasics Tool !"
-          
+echo"_____"
+echo"_____"
+echo"_____"
+echo"_____"
+echo"_____"
           
 echo "1 - NPhish Tool"
 echo "2 - zphisher Tool"
 echo "3 - install All termux Basics"
-USER_INPUT
 
 #if statment
-read -p "Enter a string: " user
+read -p "Enter a number : " user
 
 nphish="1"
 
 if [ "$user" = "$nphish" ]; then
 
-    echo "$NPhish"
+    NPhish
 
 fi
 
-read -p "Enter a string: " user
+read -p "Enter a number: " user
 
 zphisher="2"
 
 if [ "$user" = "$zphisher" ]; then
 
-    echo "$zphisher"
+    zphisher
 
 fi
 
-read -p "Enter a string: " user
+read -p "Enter a number: " user
 
 TermuxPkgs="3"
 
 if [ "$user" = "$TermuxPkgs" ]; then
 
-    echo "$pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrade"
-
+    pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrade
 fi
