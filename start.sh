646f9e9692ce314cbea97cc6ea4adc641efeca01
OfficalAptx-4869 <133824228+OfficalAptx-4869@users.noreply.github.com>
2023-05-19 22:23:59 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -23,31 +23,20 @@ echo "2 - zphisher Tool"
 echo "3 - install All termux Basics"
 
 #if statment
-read -p "Enter a number : " user
+function console_response() {
 
-nphish="1"
+    if [[ $1 -eq 1 ]]; then
 
-if [ "$user" = "$nphish" ]; then
-pip install NPhish
-NPhish
+        NPhish
 
-fi
+    elif [[ $1 -eq 2 ]]; then
 
-read -p "Enter a number: " user
+        zphisher
 
-zphisher="2"
+    elif [[ $1 -eq 3 ]]; then
 
-if [ "$user" = "$zphisher" ]; then
+        pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrade
 
-    zphisher
+    fi
 
-fi
-
-read -p "Enter a number: " user
-
-TermuxPkgs="3"
-
-if [ "$user" = "$TermuxPkgs" ]; then
-
-    pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrade
-fi
+}
