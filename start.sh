c42ef1eeaffa9dee48a8909df189dd6688c305b3
OfficalAptx-4869 <133824228+OfficalAptx-4869@users.noreply.github.com>
2023-05-19 16:30:54 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,16 @@
 
 #installation of tools
 apt update && apt upgrade -y
-pip install NPhish
 pkg install tur-repo
+clear
+echo "Please press any button"
+read
 pkg install zphisher
 clear
-echo "press any button to start !"
+echo "Please press any button"
+pip install NPhish
+clear
+echo "press any button to start the tool !"
 read
 clear
 
@@ -25,32 +30,21 @@ termuxbasics=3
 #if statment
 
 if [ $NPhish -eq 1 ]
-
 then
-
 NPhish
-
 cd
-
 fi
 
 if [ $zphisher -eq 2 ]
-
 then
-
 zphisher
-
 cd
-
 fi
 
 if [ $termuxbasics -eq 3 ]
-
 then
-
 pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrade
 clear
 echo "All Termux Basics & packages installed"
 echo "thanks for using AptxBasics"
-
 fi
