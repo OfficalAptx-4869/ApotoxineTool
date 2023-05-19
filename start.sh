e11db94284b5252e27ba2e434c26d45ec72de838
OfficalAptx-4869 <133824228+OfficalAptx-4869@users.noreply.github.com>
2023-05-19 14:28:19 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,33 +18,23 @@ echo " AptxBasics Tool !
 
 echo "1 - NPhish Tool"
 read NPhish
+echo "2 - zphisher Tool"
+read zphisher
+echo "3 - install All termux Basics"
+read TermuxBasics
 if [$NPhish = 1]
 
 then
 
-cd
-
 NPhish
 
-echo "2 - zphisher Tool"
-read zphisher
+cd
+exit
 
 if [$zphisher = 2]
 
 then
 
-cd
-
 zphisher
 
-echo "3 - install All termux Basics"
-read TermuxBasics
-if [$TermuxBasics = 3]
-
-then
-pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrade
-clear
-./start.sh
-echo "ALL TERMUX BASICS INSTALLED"
-echo "Thank You for using AptxBasics"
-
+cd
