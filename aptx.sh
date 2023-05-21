ef111f56ef33b65db970314d3551a758fdbfaef8
OfficalAptx-4869 <133824228+OfficalAptx-4869@users.noreply.github.com>
2023-05-21 23:49:48 +0300

    Update aptx.sh

--- aptx.sh
+++ aptx.sh
@@ -40,7 +40,7 @@ echo -e "\e[32m
 echo ""
 echo ""
 echo -e "\e[31mNote : some tools requires installing all
-termux packages so please choose number 3
+termux packages so please choose number 5
 before choosing any tool to run !\e[0m"
 
 
