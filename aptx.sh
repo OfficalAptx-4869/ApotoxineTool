2e2e10095ae30fb2b2a6f7a8b67e0d019cc7303c
OfficalAptx-4869 <133824228+OfficalAptx-4869@users.noreply.github.com>
2023-05-22 00:03:42 +0300

    Update aptx.sh

--- aptx.sh
+++ aptx.sh
@@ -217,12 +217,12 @@ echo "did you install qeum system ?"
 
 read -p "(y/n) :" QeumVerify
 
-if [ "$QeumVerify" == "y" - "Y" ]; then
+if [ "$QeumVerify" == "Y" ]; then
 echo -e "\e[33mExample : storage/emulated/0/download/(file name.img)\e[0m"
 echo -e "\e[33mwindows file extension must be (.img / .qcow2)\e[0m"
 read -p "Enter Windows file path : " path
 qemu-system-i386 -hda "$path" -vnc 127.0.0.1:1
-elif [ "$QeumVerify" == "n" - "N" ]; then
+elif [ "$QeumVerify" == "N" ]; then
 
     clear
 
@@ -234,6 +234,47 @@ elif [ "$QeumVerify" == "n" - "N" ]; then
     clear
     echo "Wrong Input"
     ./aptx.sh
+fi 
+elif [[ $user == 10 ]] then 
+
+clear
+
+echo "NOTE : Running Windows on mobile requires installing qeum system"
+
+echo ""
+
+echo "did you install qeum system ?"
+
+read -p "(y/n) :" QeumVerify
+
+if [ "$QeumVerify" == "y" ]; then
+
+echo -e "\e[33mExample : storage/emulated/0/download/(file name.img)\e[0m"
+
+echo -e "\e[33mwindows file extension must be (.img / .qcow2)\e[0m"
+
+read -p "Enter Windows file path : " path
+
+qemu-system-i386 -hda "$path" -vnc 127.0.0.1:1
+
+elif [ "$QeumVerify" == "n" ]; then
+
+    clear
+
+    cd
+
+    echo -e "\e[33mplease run the command (./aptx.sh) and install 
+
+    qeum system by choosing the number (9) !\e[0m"
+
+    else
+
+    clear
+
+    echo "Wrong Input"
+
+    ./aptx.sh
+
 fi 
 elif [[ $user == 99 ]] then
 clear
